e2ceeda7cc33c65de5dcab7177979f7f87fef3d2
Ssameera123 <shaiksameera581@gmail.com>
2024-07-05 12:24:09 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <div className="navbar-brand">Annamachary</div>
+            <div className="navbar-links">
+                <a href="#">Home</a>
+                <span>|</span>
+                <a href="#">About</a>
+                <span>|</span>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+    );
+};
+export default Navbar;
---
+++ home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+    <h1 className="Home">Home Page Content</h1>
+    );
+
+}
+export default Home;

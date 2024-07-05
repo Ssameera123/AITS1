d6efbfad8f258ddb2844786c6960a42737e13f9e
Ssameera123 <shaiksameera581@gmail.com>
2024-07-05 12:30:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg'; 
-import './App.css'; 
-import './css/Custom.css'; 
-import Navbar from './components/Navbar'; import Content from './components/Content'; import Sidebar from './components/Sidebar'; 
-function App() { 
-return ( 
-<div className="App"> 
-<div> 
-<Navbar /> 
-<Content /> 
-<Sidebar /> 
-</div> 
-</div> 
-); 
-} 
+import logo from 'react';
+import {Routes,Route,Link} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Notfound from './Components/Notfound';
+function App() {
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="\">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+          <Link to="/Contact">Contact</Link> 
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" elements={<Home />} />
+        <Route path="/About" elements={<About />} />
+        <Route path="/Contact" elements={<Contact />} />
+        <Route path="=" elements={<Notfound />} />
+      </Routes>
+    </div>
+  )
+}
 export default App; 
